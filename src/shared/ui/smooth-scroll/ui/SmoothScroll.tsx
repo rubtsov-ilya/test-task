cd8e1a491e6b245029b1292d1e1874461d26c605
rubtsov-ilya <rubtsovilya398@gmail.com>
2026-06-30 04:23:53 +0300

    feat: add scrollbar lib from task

---
+++ src/shared/ui/smooth-scroll/ui/SmoothScroll.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { FC, ReactNode, useEffect, useRef } from 'react';
+import Scrollbar from 'smooth-scrollbar';
+import styles from './SmoothScroll.module.scss';
+
+
+interface SmoothScrollProps {
+  children: ReactNode;
+}
+
+export const SmoothScroll: FC<SmoothScrollProps> = ({ children }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    if (!container) return;
+
+    const scrollbar = Scrollbar.init(container, {
+      damping: 0.08,
+      alwaysShowTracks: false,
+      thumbMinSize: 20,
+    });
+
+    return () => {
+      if (scrollbar) {
+        scrollbar.destroy();
+      }
+    };
+  }, []);
+
+  return (
+    <div
+      ref={containerRef}
+      className={styles['smooth-scroll']}
+    >
+      {children}
+    </div>
+  );
+};
